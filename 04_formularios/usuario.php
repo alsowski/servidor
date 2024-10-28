c432cc2c068e25b02183b5067e2028fa534cc395
alsowski <alevilca05@gmail.com>
2024-10-28 14:29:11 +0100

    28/10/2024

--- 04_formularios/usuario.php
+++ 04_formularios/usuario.php
@@ -13,20 +13,63 @@
 <body>
     <div class="container">
     <!-- Content here -->
+
+    <?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $tmp_usuario = $_POST["usuario"];
+        $tmp_nombre = $_POST["nombre"];
+        $tmp_apellidos = $_POST["apellidos"];
+
+        if($tmp_usuario == '') {
+            $err_usuario = "El usuario es obligatorio";
+        } else {
+            // letras de la A a la Z (mayus o minus), números y barrabaja (4-12 chars)
+            $patron = "/$[a-zA-Z0-9_]{4-12}^/";
+            if(!preg_match($patron, $tmp_usuario)) {
+                $err_usuario = "El usuario debe contener de 4 a 12 letras,
+                números o barrabaja";
+            } else {
+                $usuario = $tmp_usuario;
+            }
+        }
+
+        if($tmp_nombre == '') {
+            $err_nombre = "El nombre es obligatorio";
+        } else {
+            if(strlen($tmp_nombre) < 2 || strlen($tmp_nombre) > 40) {
+                $err_nombre = "El nombre debe tener 2 y 40 carácteres";
+            } else {
+                // letras, espacios en blanco y tildes
+                $patron = "/^[a-zA-Z áéíóúÁÉÍÓÚñÑüÜ]$/";
+                if(!preg_match($patron,  $tmp_nombre)) {
+                    $err_nombre = "El nombre solo puede contener letras y espacios
+                    en blanco";
+                } else {
+                    $nombre = $tmp_nombre;
+                }
+            }
+        }
+    }
+    ?>
+
         <h1>Formulario usuario</h1>
     
-        <form class="col-9" action="" method="post">
+        <form class="col-4" action="" method="post">
             <div class="mb-3">
                 <label class="form-label">Usuario</label>
-                <input class="form-control" type="text">
+                <input class="form-control" type="text" name="usuario">
+                <?php if(isset($err_usuario)) echo "<span class='error'>$err_usuario</span>" ?>
             </div>
             <div class="mb-3">
                 <label class="form-label">Nombre</label>
-                <input class="form-control" type="text">
+                <input class="form-control" type="text" name="nombre">
             </div>
             <div class="mb-3">
                 <label class="form-label">Apellidos</label>
-                <input class="form-control" type="text">
+                <input class="form-control" type="text" name="apellidos">
+            </div>
+            <div class="mb-3">
+                <input class="btn btn-primary" type="submit" value="Enviar">
             </div>
         </form>
     </div>
