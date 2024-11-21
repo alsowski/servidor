b39b0dcab56cbf4e5af0724315d0d54281f829e4
alsowski <alevilca05@gmail.com>
2024-11-21 14:11:10 +0100

    21/11/2024

--- 06_bases_de_datos/animes/nuevo_anime.php
+++ 06_bases_de_datos/animes/nuevo_anime.php
@@ -18,14 +18,6 @@
 </head>
 <body>
     <?php
-        function depurar(string $entrada) : string {
-            $salida = htmlspecialchars($entrada);
-            $salida = trim($salida);
-            $salida = stripslashes($salida);
-            $salida = preg_replace('!\s+!', ' ', $salida);
-            return $salida;
-        }
-
         $estudios = [
             'Madhouse',
             'Toei Animation',
@@ -37,91 +29,74 @@
     <div class="container">
         <?php
         if($_SERVER["REQUEST_METHOD"] == "POST") {
-            $tmp_titulo = depurar($_POST["titulo"]);
-            $tmp_nombre_estudio = depurar($_POST["nombre_estudio"]);
-            $tmp_anno_estreno = depurar($_POST["anno_estreno"]);
-            $tmp_num_temporadas = depurar($_POST["num_temporadas"]);
+            $titulo = ($_POST["titulo"]);
+            $nombre_estudio = ($_POST["nombre_estudio"]);
+            $anno_estreno = ($_POST["anno_estreno"]);
+            $num_temporadas = ($_POST["num_temporadas"]);
 
-            if($tmp_titulo == '') {
-                $err_titulo = "El titulo es obligatorio";
-            } else {
-                if(strlen($tmp_titulo) > 80) {
-                    $err_titulo = "El titulo debe contener menos de 80 carácteres";
-                } else {
-                    $titulo = $tmp_titulo;
-                }
-            }
+            /**
+             * $_FILES -> que es uun array BIDIMENSIONAL
+             */
+            // var_dump($_FILES["imagen"]);
+            $nombre_imagen = $_FILES["imagen"]["name"];
+            $ubicacion_temporal = $_FILES["imagen"]["tmp_name"];
+            $ubicacion_final = "./imagenes/$nombre_imagen";
 
-            if($tmp_nombre_estudio == '') {
-                $err_nombre_estudio = "El nombre del estudio es obligatorio";
-            } else {
-                $nombre_estudio = $tmp_nombre_estudio;
-            }
+            move_uploaded_file($ubicacion_temporal, $ubicacion_final);
 
-            if ($tmp_anno_estreno && ($tmp_anno_estreno < 1960 || $tmp_anno_estreno > (date("Y") + 5))) {
-                $err_anno_estreno = "El año de estreno no es válido";
-            } else {
-                $anno_estreno = $tmp_anno_estreno;
-            }
+            $sql = "INSERT INTO animes (titulo, nombre_estudio, anno_estreno, num_temporadas, imagen)
+                VALUES ('$titulo', '$nombre_estudio', $anno_estreno, $num_temporadas, '$ubicacion_final')";
 
-            if($tmp_num_temporadas == ""){
-                $err_num_temporadas = "El número de temporadas es obligatorio";
-            } else {
-                if ($tmp_num_temporadas < 1 || $tmp_num_temporadas > 99) {
-                    $err_num_temporadas = "El número de temporadas no es válido";
-                } else {
-                    $num_temporadas = $tmp_num_temporadas;
-                }
-            }
-
-        $sql = "INSERT INTO animes (titulo, nombre_estudio, anno_estreno, num_temporadas)
-            VALUES ('$titulo', '$nombre_estudio', $anno_estreno, $num_temporadas)";
-
-        $_conexion -> query($sql);
+            $_conexion -> query($sql);
         }
+
+        $sql = "SELECT * FROM estudios ORDER BY nombre_estudio";
+        $resultado = $_conexion -> query($sql);
+        $estudios = [];
+
+        while($fila = $resultado -> fetch_assoc()) {
+            array_push($estudios, $fila["nombre_estudio"]);
+        }
+        // print_r($estudios);
         ?>
 
     <h1>Formulario Anime</h1>
 
-        <form class="col-6" action="" method="post">
+        <form class="col-6" action="" method="post" enctype="multipart/form-data">
             <div class="mb-3">
                 <label class="form-label">Titulo</label>
                 <input class="form-control" type="text" name="titulo">
-                <?php if(isset($err_titulo)) echo "<span class='error'>$err_titulo</span>" ?>
             </div>
             <div class="mb-3">
                 <label class="form-label">Nombre Estudio</label>
-                <select name="nombre_estudio">
-                <?php
-                    foreach($estudios as $estudio) {
-                        echo "<option value=".strtolower($estudio).">".$estudio."</option>";
-                    }
-                ?>
+                <select class="form-select" name="nombre_estudio">
+                    <option value="" selected disabled hidden>--- Elige el estudio ---</option>
+                    <?php 
+                    foreach($estudios as $estudio) { ?>
+                        <option value="<?php echo $estudio ?>">
+                            <?php echo $estudio ?>
+                        </option>
+                    <?php } ?>
                 </select>
-                <?php if(isset($err_nombre_estudio)) echo "<span class='error'>$err_nombre_estudio</span>"; ?>
             </div>
             <div class="mb-3">
                 <label class="form-label">Año de estreno</label>
                 <input class="form-control" type="text" name="anno_estreno">
-                <?php if(isset($err_anno_estreno)) echo "<span class='error'>$err_anno_estreno</span>" ?>
             </div>
             <div class="mb-3">
                 <label class="form-label">Número de temporadas</label>
                 <input class="form-control" type="text" name="num_temporadas">
-                <?php if(isset($err_num_temporadas)) echo "<span class='error'>$err_num_temporadas</span>" ?>
             </div>
             <div class="mb-3">
-                <input class="btn btn-primary" type="submit" value="Enviar">
+                <label class="form-label">Imagen</label>
+                <input class="form-control" type="file" name="imagen">
+            </div>
+            <div class="mb-3">
+                <input class="btn btn-primary" type="submit" value="Insertar">
+                <a class="btn btn-secondary" href="index.php">Volver</a>
             </div>
         </form>
-        <?php
-            if(isset($titulo) && isset($nombre_estudio) && isset($anno_estreno) && isset($num_temporadas)){ ?>
-                <p><?php echo $titulo ?></p>
-                <p><?php echo $nombre_estudio ?></p>
-                <p><?php echo $anno_estreno ?></p>
-                <p><?php echo $num_temporadas ?></p>
-            <?php } ?>
-        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
