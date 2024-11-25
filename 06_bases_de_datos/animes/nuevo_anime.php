d28a8454dd934724a4a6711033ba5851870c01da
alsowski <alevilca05@gmail.com>
2024-11-25 14:25:05 +0100

    registro 25/11

--- 06_bases_de_datos/animes/nuevo_anime.php
+++ 06_bases_de_datos/animes/nuevo_anime.php
@@ -3,48 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Anime</title>
+    <title>Nuevo anime</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <?php
         error_reporting( E_ALL );
-        ini_set("display_errors", 1 );  
+        ini_set("display_errors", 1 );    
+
         require('conexion.php');
-    ?>
-    <style>
-        .error {
-            color: red;
+
+        session_start();
+        if(isset($_SESSION["usuario"])) {
+            echo "<h2>Bienvenid@ " . $_SESSION["usuario"] . "</h2>";
+        }else{
+            header("location: usuario/iniciar_sesion.php");
+            exit;
         }
-    </style>
+    ?>
 </head>
 <body>
-    <?php
-        $estudios = [
-            'Madhouse',
-            'Toei Animation',
-            'Bones',
-            'Kyoto Animation',
-            'Studio Ghibli'
-        ];
-    ?>
     <div class="container">
+        <h1>Nuevo anime</h1>
         <?php
         if($_SERVER["REQUEST_METHOD"] == "POST") {
-            $titulo = ($_POST["titulo"]);
-            $nombre_estudio = ($_POST["nombre_estudio"]);
-            $anno_estreno = ($_POST["anno_estreno"]);
-            $num_temporadas = ($_POST["num_temporadas"]);
-
+            $titulo = $_POST["titulo"];
+            $nombre_estudio = $_POST["nombre_estudio"];
+            $anno_estreno = $_POST["anno_estreno"];
+            $num_temporadas = $_POST["num_temporadas"];
             /**
-             * $_FILES -> que es uun array BIDIMENSIONAL
+             * $_FILES -> que es un array BIDIMENSIONAL
              */
-            // var_dump($_FILES["imagen"]);
+            //var_dump($_FILES["imagen"]);
             $nombre_imagen = $_FILES["imagen"]["name"];
             $ubicacion_temporal = $_FILES["imagen"]["tmp_name"];
             $ubicacion_final = "./imagenes/$nombre_imagen";
 
             move_uploaded_file($ubicacion_temporal, $ubicacion_final);
 
-            $sql = "INSERT INTO animes (titulo, nombre_estudio, anno_estreno, num_temporadas, imagen)
+            $sql = "INSERT INTO animes (titulo, nombre_estudio, anno_estreno, num_temporadas, imagen) 
                 VALUES ('$titulo', '$nombre_estudio', $anno_estreno, $num_temporadas, '$ubicacion_final')";
 
             $_conexion -> query($sql);
@@ -57,21 +52,19 @@
         while($fila = $resultado -> fetch_assoc()) {
             array_push($estudios, $fila["nombre_estudio"]);
         }
-        // print_r($estudios);
+        //print_r($estudios);
+ 
         ?>
-
-    <h1>Formulario Anime</h1>
-
         <form class="col-6" action="" method="post" enctype="multipart/form-data">
             <div class="mb-3">
-                <label class="form-label">Titulo</label>
+                <label class="form-label">Título</label>
                 <input class="form-control" type="text" name="titulo">
             </div>
             <div class="mb-3">
-                <label class="form-label">Nombre Estudio</label>
+                <label class="form-label">Nombre estudio</label>
                 <select class="form-select" name="nombre_estudio">
                     <option value="" selected disabled hidden>--- Elige el estudio ---</option>
-                    <?php 
+                    <?php
                     foreach($estudios as $estudio) { ?>
                         <option value="<?php echo $estudio ?>">
                             <?php echo $estudio ?>
@@ -80,7 +73,7 @@
                 </select>
             </div>
             <div class="mb-3">
-                <label class="form-label">Año de estreno</label>
+                <label class="form-label">Año estreno</label>
                 <input class="form-control" type="text" name="anno_estreno">
             </div>
             <div class="mb-3">
